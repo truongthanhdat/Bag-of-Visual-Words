bce0951c4a4249000d33a2fb4f5b8d5bfc4023bf
truongthanhdat <1512107@student.hcmus.edu.vn>
2017-12-10 19:39:20 +0700

    Bag of Visual Words

--- src/BoW.py
+++ src/BoW.py
@@ -44,10 +44,18 @@ def computeNewCentroids(points, centroids):
         newCentroids[i] = np.mean(points[index == i], axis=0)
     return newCentroids
 
-def findClusterring(K, points):
+def computeCostCluster(points, centroids):
+    index = cluster(points, centroids)
+    dists = points - centroids[index]
+    cost = 0.0
+    for dist in dists:
+        cost = cost + np.sqrt(np.inner(dist, dist))
+    return cost
+
+def findClustering(K, points):
     lenFeatures = points.shape[1]
     N = points.shape[0]
-    index = random.sample(np.argange(N))
+    index = random.sample(np.arange(N), K)
     centroids = np.zeros((K, lenFeatures))
     for i in xrange(K):
         centroids[i] = points[index[i]].copy()
@@ -59,6 +67,8 @@ def findClusterring(K, points):
         if (error <= THRESHOLD):
             break
         centroids = newCentroids
+        print 'Clustering Cost', computeCostCluster(points, centroids)
+
     return centroids
 
 def featuresExtracting(filePath):
@@ -66,12 +76,32 @@ def featuresExtracting(filePath):
     features = []
     labels = []
     for line in file:
-        imagePath = 'oxbuild_images/' + line[:-1]
+        imagePath = '../data/oxbuild_images/' + line[:-1]
         labels.append(line[:-5])
         feats = computeFeatures(imagePath)
+        if feats is None:
+            continue
         for feat in feats:
             #norm = np.sqrt(np.inner(feat, feat))
             features.append(feat)
     features = np.array(features)
     return features, labels
+
+if __name__ == '__main__':
+    #Extract Features
+    duration = time.time()
+    #[features, labels] = featuresExtracting('../data/META')
+    #np.save('../data/features/features', features)
+    #np.save('../data/features/labels, labels)
+    features = np.load('../data/features/features.npy')
+    labels = np.load('../data/features/labels.npy')
+    duration = time.time() - duration
+    print 'Extracting Features take', duration, 'seconds'
+
+    #K-Means
+    K = int(features.shape[0]) / 100
+    duration = time.time()
+    centroids = findClustering(K, features)
+    duration = time.time() - duration
+    print 'K-Means takkes', duration, 'seconds'
 
